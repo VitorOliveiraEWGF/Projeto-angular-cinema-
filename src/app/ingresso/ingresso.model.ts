69787ddf6f372963fef6780b997c37dcd7067b3d
VitorOliveiraEWGF <135755701+VitorOliveiraEWGF@users.noreply.github.com>
2024-05-18 13:27:15 -0300

    PROJETO ANGULAR CINEMA

---
+++ src/app/ingresso/ingresso.model.ts
@@ -0,0 +1,13 @@
+
+
+export class ingressomodel {
+  nome: string;
+  quantidade: number;
+  filmes: string;
+
+  constructor(nome: string, quantidade: number, filme: string) {
+    this.nome = nome;
+    this.quantidade = quantidade;
+    this.filmes = filme;
+  }
+}
